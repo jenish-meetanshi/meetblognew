fd0a7e668592c4176a135ea3daaaee0e70ee5a9f
jenish-meetanshi <jenish.dholakiya@meetanshi.com>
2024-11-27 15:43:15 +0530

    Update fullcode

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,47 +8,12 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 
-// module.exports = {
-//   siteMetadata: {
-//     siteUrl: `https://5d43103688.nxcli.io/`,
-//     pathPrefix: "/blog/testwordpress/public",
-//     title: `My Gatsby WordPress Site`,
-//   },
-//   plugins: [
-//     {
-//       resolve: `gatsby-source-wordpress`,
-//       options: {
-//         url: `https://mitfestival.app/meetanshiblog/graphql`,
-//         schema: {
-//           timeout: 30000,
-//         },
-//         develop: {
-//           hardCacheMediaFiles: true,
-//         },
-//         type: {
-//           MediaItem: {
-//             createFileNodes: false, // Disable file downloads
-//           },
-//           User: {
-//             // Fetch ACF fields for User type
-//             excludeFieldNames: [],
-//           },
-//         },
-//       },
-//     },
-//     `gatsby-plugin-image`,
-//     `gatsby-plugin-sharp`,
-//     `gatsby-transformer-sharp`,
-//     "gatsby-plugin-netlify",
-//   ],
-// };
-
 module.exports = {
   siteMetadata: {
     siteUrl: `https://5d43103688.nxcli.io/`,
     title: `My Gatsby WordPress Site`,
   },
-  pathPrefix: `/blog/testwordpress`,
+  pathPrefix: `/blog/testwordpress/public`,
   plugins: [
     {
       resolve: `gatsby-source-wordpress`,
