5fe9145149151f3564e33797f35dcba76843eb74
jenish-meetanshi <jenish.dholakiya@meetanshi.com>
2024-12-19 10:33:08 +0530

    Update Header.js 1234

--- src/components/Header.js
+++ src/components/Header.js
@@ -24,6 +24,67 @@ const Header = () => {
               </Link>
             </div>
 
+
+            {/* Navigation Menu */}
+            <ul className="navbar-nav">
+              <li className="nav-item dropdown">
+                <Link to="#" className="nav-link dropdown-toggle" role="button">
+                  Extensions
+                </Link>
+                <ul className="dropdown-menu">
+                  <li>
+                    <Link to="/magento-2-extensions.html" className="dropdown-item">Magento 2 Extensions</Link>
+                  </li>
+                  <li>
+                    <Link to="/graphql-compatible-magento-2-extensions.html" className="dropdown-item">GraphQL Compatible M2 Extensions</Link>
+                  </li>
+                  <li>
+                    <Link to="/magento-extensions.html" className="dropdown-item">Magento Extensions</Link>
+                  </li>
+                  <li>
+                    <Link to="/free-magento-extensions.html" className="dropdown-item">Free Magento Extensions</Link>
+                  </li>
+                  <li>
+                    <Link to="/hyva-theme-magento-2-extensions.html" className="dropdown-item">Magento 2 Hyva Theme Extensions</Link>
+                  </li>
+                  <li>
+                    <Link to="/shopify-apps.html" className="dropdown-item">Shopify Apps</Link>
+                  </li>
+                </ul>
+              </li>
+
+              <li className="nav-item dropdown">
+                <Link to="#" className="nav-link dropdown-toggle" role="button">
+                  Services
+                </Link>
+                <ul className="dropdown-menu">
+                  <li>
+                    <Link to="/magento-development-services.html" className="dropdown-item">Magento Development Services</Link>
+                  </li>
+                  <li>
+                    <Link to="/migration-services.html" className="dropdown-item">Migration Services</Link>
+                  </li>
+                  <li>
+                    <Link to="/hire-us.html" className="dropdown-item">Hire Us</Link>
+                  </li>
+                  <li>
+                    <Link to="/shopify-services.html" className="dropdown-item">Shopify Services</Link>
+                  </li>
+                  <li>
+                    <Link to="/shopify-migration-service.html" className="dropdown-item">Shopify Migration Service</Link>
+                  </li>
+                </ul>
+              </li>
+
+              <li className="nav-item">
+                <Link to="/about" className="nav-link">About</Link>
+              </li>
+
+              <li className="nav-item">
+                <Link to="/contacts" className="nav-link">Contact</Link>
+              </li>
+            </ul>
+
             {/* Search Section */}
             <div className="header-search">
               <form action="/search" method="GET" className="search-form">
