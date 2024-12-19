54e04ad01686feb7f3020055b3e4cf6c5e889487
jenish-meetanshi <jenish.dholakiya@meetanshi.com>
2024-12-19 12:34:16 +0530

    Update gatsby-browser.js Poppins

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,3 +4,13 @@ import "./src/styles/global.css"; // Import your custom global styles
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
+
+export const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <link
+      key="google-fonts"
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap"
+    />,
+  ]);
+};
