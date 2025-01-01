26de37f3171781ab0ed97feb5d7b9a547c88fa9e
jenish-meetanshi <jenish.dholakiya@meetanshi.com>
2025-01-01 11:54:38 +0530

    Delete gatsby-ssr.js

--- gatsby-ssr.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export const onRenderBody = ({ setHeadComponents }) => {
-  setHeadComponents([
-    <meta key="robots" name="robots" content="noindex, nofollow" />,
-    <meta key="googlebot" name="googlebot" content="noindex, nofollow" />,
-  ]);
-};
