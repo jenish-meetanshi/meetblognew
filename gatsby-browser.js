0dbccf7fbbcd147022a5245a6e3a2275d900dad5
jenish-meetanshi <jenish.dholakiya@meetanshi.com>
2025-01-01 10:15:00 +0530

    Update gatsby-browser.js noindex

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,4 @@
-
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS globally
 import "./src/styles/global.css"; // Import your custom global styles
 import { config } from '@fortawesome/fontawesome-svg-core'
