15c44da2977e46e53ac1277b6b6d1fe05fb0dae5
jenish-meetanshi <jenish.dholakiya@meetanshi.com>
2025-01-01 11:44:22 +0530

    Update gatsby-ssr.js update

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,8 +1,6 @@
 import React from "react";
 
-const React = require("react");
-
-exports.onRenderBody = ({ setHeadComponents }) => {
+export const onRenderBody = ({ setHeadComponents }) => {
   setHeadComponents([
     <meta key="robots" name="robots" content="noindex, nofollow" />,
     <meta key="googlebot" name="googlebot" content="noindex, nofollow" />,
