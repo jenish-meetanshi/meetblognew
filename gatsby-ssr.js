23447022f8fe1d23621abf6586e147bc96303cd5
jenish-meetanshi <jenish.dholakiya@meetanshi.com>
2025-01-16 00:01:25 +0530

    Update gatsby-ssr.js fa

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,5 +4,10 @@ export const onRenderBody = ({ setHeadComponents }) => {
   setHeadComponents([
     <meta key="robots" name="robots" content="noindex, nofollow" />,
     <meta key="googlebot" name="googlebot" content="noindex, nofollow" />,
+    <link
+      key="font-awesome"
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
+    />,
   ]);
 };
