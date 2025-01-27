f84e6ffc87342dbb4d4b4fe33c001817f00de852
jenish-meetanshi <jenish.dholakiya@meetanshi.com>
2025-01-27 11:06:22 +0530

    Update gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -11,18 +11,3 @@ export const onRenderBody = ({ setHeadComponents }) => {
     />,
   ]);
 };
-
-exports.onRenderBody = ({ setPostBodyComponents }) => {
-  setPostBodyComponents([
-    <script
-      key="gatsby-script-loader"
-      dangerouslySetInnerHTML={{
-        __html: `
-          /*<![CDATA[*/
-          window.pagePath = "/blog" + window.pagePath;
-          /*]]>*/
-        `,
-      }}
-    />,
-  ]);
-};
