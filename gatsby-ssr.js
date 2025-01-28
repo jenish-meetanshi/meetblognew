014ee8f9ca6d9893a3a607c31a72c13b0177a9c0
jenish-meetanshi <jenish.dholakiya@meetanshi.com>
2025-01-28 15:58:24 +0530

    Update gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const onRenderBody = ({ setHeadComponents }) => {
+export const onRenderBody = ({ setHeadComponents, setBodyComponents }) => {
   setHeadComponents([
     <meta key="robots" name="robots" content="noindex, nofollow" />,
     <meta key="googlebot" name="googlebot" content="noindex, nofollow" />,
@@ -9,5 +9,9 @@ export const onRenderBody = ({ setHeadComponents }) => {
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
     />,
+  ]);
+
+  setBodyComponents([
+   <script key="gatsby-script-loader" id="gatsby-script-loader"></script>,
   ]);
 };
