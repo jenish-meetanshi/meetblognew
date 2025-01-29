ee2328259a0f31535fa8857262cb5bbbc9c21c96
jenish-meetanshi <jenish.dholakiya@meetanshi.com>
2025-01-29 09:43:00 +0530

    Update gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,33 +1,65 @@
+
 import React from "react";
 
-export const onRenderBody = ({ setHeadComponents}) => {
-  setHeadComponents([
-    <meta key="robots" name="robots" content="noindex, nofollow" />,
-    <meta key="googlebot" name="googlebot" content="noindex, nofollow" />,
-    <link
-      key="font-awesome"
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
-    />,
-  ]);
-};
+// export const onRenderBody = ({ setHeadComponents}) => {
+//   setHeadComponents([
+//     <meta key="robots" name="robots" content="noindex, nofollow" />,
+//     <meta key="googlebot" name="googlebot" content="noindex, nofollow" />,
+//     <link
+//       key="font-awesome"
+//       rel="stylesheet"
+//       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
+//     />,
+//   ]);
+// };
 
-export const onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
-  let headComponents = getHeadComponents();
+// export const onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
+//   let headComponents = getHeadComponents();
 
+//   // Remove the existing Gatsby script
+//   headComponents = headComponents.filter(
+//     (component) =>
+//       !(
+//         component.type === "script" &&
+//         component.props.id === "gatsby-script-loader"
+//       )
+//   );
+
+//   // Add the modified script
+//   headComponents.push(
+//     <script key="gatsby-script-loader" id="gatsby-script-loader"></script>
+//   );
+
+//   replaceHeadComponents(headComponents);
+// };
+
+
+export const onPreRenderHTML = ({ 
+  getPostBodyComponents,
+  replacePostBodyComponents,
+}) => {
+  let postBodyComponents = getPostBodyComponents();
+  
   // Remove the existing Gatsby script
-  headComponents = headComponents.filter(
+  postBodyComponents = postBodyComponents.filter(
     (component) =>
       !(
         component.type === "script" &&
         component.props.id === "gatsby-script-loader"
       )
   );
-
+  
   // Add the modified script
-  headComponents.push(
+  postBodyComponents.push(
     <script key="gatsby-script-loader" id="gatsby-script-loader"></script>
   );
+  
+  replacePostBodyComponents(postBodyComponents);
+};
 
-  replaceHeadComponents(headComponents);
+// Alternative approach using onRenderBody if above doesn't work
+export const onRenderBody = ({ setPostBodyComponents }) => {
+  setPostBodyComponents([
+    <script key="gatsby-script-loader" id="gatsby-script-loader" />
+  ]);
 };
