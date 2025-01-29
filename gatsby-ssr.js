1f55531e1645cc68b623440509c5a1f69970ec59
jenish-meetanshi <jenish.dholakiya@meetanshi.com>
2025-01-29 09:35:12 +0530

    Update gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,16 +1,16 @@
 import React from "react";
 
-// export const onRenderBody = ({ setHeadComponents}) => {
-//   setHeadComponents([
-//     <meta key="robots" name="robots" content="noindex, nofollow" />,
-//     <meta key="googlebot" name="googlebot" content="noindex, nofollow" />,
-//     <link
-//       key="font-awesome"
-//       rel="stylesheet"
-//       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
-//     />,
-//   ]);
-// };
+export const onRenderBody = ({ setHeadComponents}) => {
+  setHeadComponents([
+    <meta key="robots" name="robots" content="noindex, nofollow" />,
+    <meta key="googlebot" name="googlebot" content="noindex, nofollow" />,
+    <link
+      key="font-awesome"
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
+    />,
+  ]);
+};
 
 export const onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
   let headComponents = getHeadComponents();
