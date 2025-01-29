85d98ff34b25db8a59927fcfbb7d125b62c3b17a
jenish-meetanshi <jenish.dholakiya@meetanshi.com>
2025-01-29 09:31:08 +0530

    Update gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,13 +1,33 @@
 import React from "react";
 
-export const onRenderBody = ({ setHeadComponents}) => {
-  setHeadComponents([
-    <meta key="robots" name="robots" content="noindex, nofollow" />,
-    <meta key="googlebot" name="googlebot" content="noindex, nofollow" />,
-    <link
-      key="font-awesome"
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
-    />,
-  ]);
+// export const onRenderBody = ({ setHeadComponents}) => {
+//   setHeadComponents([
+//     <meta key="robots" name="robots" content="noindex, nofollow" />,
+//     <meta key="googlebot" name="googlebot" content="noindex, nofollow" />,
+//     <link
+//       key="font-awesome"
+//       rel="stylesheet"
+//       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
+//     />,
+//   ]);
+// };
+
+export const onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
+  let headComponents = getHeadComponents();
+
+  // Remove the existing Gatsby script
+  headComponents = headComponents.filter(
+    (component) =>
+      !(
+        component.type === "script" &&
+        component.props.id === "gatsby-script-loader"
+      )
+  );
+
+  // Add the modified script
+  headComponents.push(
+    <script key="gatsby-script-loader" id="gatsby-script-loader"></script>
+  );
+
+  replaceHeadComponents(headComponents);
 };
