db815673baa6fb2585cd1f3ab1cca054d600aa5f
jenish-meetanshi <jenish.dholakiya@meetanshi.com>
2025-01-30 10:36:44 +0530

    Update and rename processPost.js to contentCleaner.js

--- src/utils/processPost.js
+++
@@ -1,9 +0,0 @@
-const processPostContent = async ({ remoteNode }) => {
-  if (remoteNode.content) {
-    remoteNode.content = remoteNode.content
-      .replace(/data-gatsby-image-wrapper=""/g, '')
-      .replace(/class="gatsby-image-wrapper[^"]*"/g, '')
-      .replace(/class="inline-gatsby-image-wrapper[^"]*"/g, '');
-  }
-  return remoteNode;
-};
