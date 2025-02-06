a3be4421b3b4b29d87c88fb0a2c7f49fbc2b6005
jenish-meetanshi <jenish.dholakiya@meetanshi.com>
2025-02-06 16:41:23 +0530

    Update post-detail.js

--- src/templates/post-detail.js
+++ src/templates/post-detail.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { graphql, Link} from "gatsby";
 import { Breadcrumb } from "gatsby-plugin-breadcrumb"; // Import Breadcrumb
 import { Helmet } from "react-helmet";
