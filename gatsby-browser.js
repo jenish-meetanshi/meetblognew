4ade0ada973256e222e636922f6bbfffab21145b
jenish-meetanshi <jenish.dholakiya@meetanshi.com>
2025-04-23 12:22:28 +0530

    Update gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,3 @@
-import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS globally
 import "./src/styles/global.css"; // Import your custom global styles
 import { config } from '@fortawesome/fontawesome-svg-core'
 import React from "react";
