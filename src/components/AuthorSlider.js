75ab26997a696b9414f1718fc664e4e6d822cb5e
jenish-meetanshi <jenish.dholakiya@meetanshi.com>
2025-04-25 11:09:58 +0530

    Update AuthorSlider.js

--- src/components/AuthorSlider.js
+++ src/components/AuthorSlider.js
@@ -2,24 +2,33 @@ import React from "react";
 import { graphql, useStaticQuery, Link } from "gatsby";
 
 const AuthorGrid = () => {
-  const data = useStaticQuery(graphql
+  const data = useStaticQuery(graphql`
     query {
       allWpUser {
         nodes {
           id
           name
+          slug
           userImage
           designation
-          slug
+          posts {
+            nodes {
+              id
+            }
+          }
         }
       }
     }
-  );
-  
-  // Filter out the admin user
-  const authors = data.allWpUser.nodes.filter(
-    (author) => author.slug !== "meetanshi-admin"
-  );
+  `);
+
+  // Filter out 'meetanshi-admin' and sort by number of posts
+  const authors = data.allWpUser.nodes
+    .filter((author) => author.slug !== "meetanshi-admin")
+    .map((author) => ({
+      ...author,
+      postCount: author.posts.nodes.length,
+    }))
+    .sort((a, b) => b.postCount - a.postCount); // Sort descending by post count
 
   return (
     <div className="author-grid-container py-5">
@@ -29,16 +38,19 @@ const AuthorGrid = () => {
             <h3 className="author-grid-title mb-4">Meet Our Authors</h3>
           </div>
         </div>
-        
+
         <div className="row">
           {authors.map((author) => (
             <div key={author.id} className="col-xl-3 col-lg-3 col-md-6 col-sm-6 mb-4">
               <div className="author-card text-center">
-                <Link to={`/author/${author.slug}`} className="text-decoration-none">
+                <Link
+                  to={`/author/${author.slug}`}
+                  className="text-decoration-none d-flex align-items-center flex-column"
+                >
                   <img
                     src={author.userImage}
-                    alt={Profile picture of ${author.name}, ${author.designation} at Meetanshi}
-                    className="author-avatar rounded-circle mb-3" 
+                    alt={`Profile picture of ${author.name}, ${author.designation} at Meetanshi`}
+                    className="author-avatar rounded-circle mb-3"
                     loading="lazy"
                   />
                   <h5 className="author-name">{author.name}</h5>
@@ -51,4 +63,5 @@ const AuthorGrid = () => {
     </div>
   );
 };
+
 export default AuthorGrid;
