498c8cbfb7faaab0f86bae6c845b50005314eb80
jenish-meetanshi <jenish.dholakiya@meetanshi.com>
2025-04-25 18:27:43 +0530

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,7 +8,7 @@ const Hero = () => {
           <div className="row">
             <div className="col-md-12">
                 <div className="home-hero-section">
-                  <h1>Get Better at E-commerce & Marketing</h1>
+                  <h1 className="mt-0">Get Better at E-commerce & Marketing</h1>
                   <p>Find expert articles on Magento, Shopify, and Digital Marketing topics</p>
                 </div>
             </div>
